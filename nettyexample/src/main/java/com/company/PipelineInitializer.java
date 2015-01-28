eaecc995aa472fa903ec85f509d52e1c3bf245c9
suresh raja <sureshraja316@gmail.com>
2015-01-28 06:32:26 +0530

    changed pipeline initializer

--- nettyexample/src/main/java/com/company/PipelineInitializer.java
+++ nettyexample/src/main/java/com/company/PipelineInitializer.java
@@ -3,9 +3,8 @@ package com.company;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
-import io.netty.handler.codec.http.router.Handler;
-import io.netty.handler.codec.http.router.Router;
 import io.netty.util.concurrent.DefaultEventExecutorGroup;
 import io.netty.util.concurrent.EventExecutorGroup;
 
@@ -13,22 +12,24 @@ import io.netty.util.concurrent.EventExecutorGroup;
  * Created by suresh on 9/1/15.
  */
 public class PipelineInitializer extends ChannelInitializer<SocketChannel> {
-	private static final Router router = new Router()
-			.GET("/hello", SampleHandler.class);
-	private static final Handler handler = new Handler(router);
+
 	public void initChannel(SocketChannel ch) {
 		int poolSize     = 4;
 		EventExecutorGroup httpExecutorThreadPool = new DefaultEventExecutorGroup(poolSize);
-		EventExecutorGroup handlerExecutorThreadPool = new DefaultEventExecutorGroup(poolSize);
 
 		//fast
-//		ChannelPipeline p = ch.pipeline()
-//				.addLast(new HttpServerCodec())
-//				.addLast(handler.name(), handler);
+		ChannelPipeline p = ch.pipeline()
+				.addLast(new HttpServerCodec())
+				.addLast(new HttpObjectAggregator(1024*1024))
+				.addLast(new SampleHandler());
+
+
 
 		//slow
-		ChannelPipeline p = ch.pipeline()
-				.addLast(httpExecutorThreadPool,new HttpServerCodec())
-				.addLast(handlerExecutorThreadPool,handler.name(), handler);
+//		ChannelPipeline p = ch.pipeline()
+//				.addLast(new HttpServerCodec())
+//				.addLast(new HttpObjectAggregator(1024*1024))
+//				.addLast(httpExecutorThreadPool,new SampleHandler());
+
 	}
 }
